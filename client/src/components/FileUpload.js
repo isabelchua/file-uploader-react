83fbb3e2865f114d5f1a430db054b7fa7c0ebbca
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-05 22:10:42 -0700

    added react files

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,16 +1,42 @@
 import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 const FileUpload = () => {
 	const [file, setFile] = useState("");
 	const [filename, setFilename] = useState("Choose File");
+	const [uploadedFile, setUploadedFile] = useState({});
 
 	const onChange = e => {
 		setFile(e.target.files[0]);
 		setFilename(e.target.files[0].name);
 	};
+
+	const onSubmit = async e => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append("file", file);
+		console.log(file);
+		try {
+			const res = await axios.post("/upload", formData, {
+				headers: {
+					"Content-Type": "multipart/form-data"
+				}
+			});
+			const { fileName, filePath } = res.data;
+
+			setUploadedFile({ fileName, filePath });
+		} catch (err) {
+			if (err.response.status === 500) {
+				console.log("There was a problem with the server");
+			} else {
+				console.log(err.response.data.msg);
+			}
+		}
+	};
+
 	return (
 		<Fragment>
-			<form>
+			<form onSubmit={onSubmit}>
 				<input type="file" id="custom" onChange={onChange} />
 				<label htmlFor="custom">{filename}</label>
 				<input type="submit" />
--- server.js
+++ server.js
@@ -5,20 +5,22 @@ const app = express();
 
 app.use(fileUpload());
 
-//upload endpoint
-
+// Upload Endpoint
 app.post("/upload", (req, res) => {
 	if (req.files === null) {
-		return res.status(400).json({ msg: "No file was uploaded" });
+		return res.status(400).json({ msg: "No file uploaded" });
 	}
+
 	const file = req.files.file;
-	file.mv(`${__direname}/client/public/uploads/${file.name}`, err => {
+
+	file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
 		if (err) {
 			console.error(err);
 			return res.status(500).send(err);
 		}
-		res.json({ fileName: file.name, filePath: `/uploads/${filename}` });
+
+		res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
 	});
 });
 
-app.listen(5000, () => console.log("Server Started"));
+app.listen(5000, () => console.log("Server Started..."));
